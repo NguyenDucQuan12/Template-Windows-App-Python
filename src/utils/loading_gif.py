61dfee553cc2981d07248c43a90b5f88801117a8
Quan <nguyenducquan2001@gmail.com>
2025-07-16 15:05:12 +0700

    Cập nhật đoạn popup loading bằng tệp gif

--- src/utils/loading_gif.py
+++ src/utils/loading_gif.py
@@ -8,43 +8,31 @@ import os,sys
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PROJECT_DIR)
 
-# Các thư viện import từ thư mục tự tạo đặt bắt đầu từ đây
 from utils.resource import resource_path
-
-
 
 # Ghi lại nhật ký hoạt động
 logger = logging.getLogger(__name__)
 
-
-
 class LoadingGifLabel(customtkinter.CTkLabel):
     """
-    Label hiển thị gif với customtkinter
+    Label hiển thị ảnh gift với customtkinter
     """
     def __init__(self, master=None, background_color="#3cb371", resize=None, text = '', **kwargs):
         super().__init__(master, **kwargs)
-
-        # Cài đặt màu nền của gif
         # self.background_color = background_color
         self.configure(text = text,fg_color = background_color )
         # self.configure(bg=background_color)
-
-        # Biến chứa kích thước và các khung hình của gif
         self.resize = resize  
         self.frames = []  
-        self.loc = 0
-        
-        # Biến chứa cửa sổ cha
-        self.parent = master
+        self.loc = 0 
         
     def load(self, im):
         """
-        Tải gif và hiển thị lên label theo vòng lặp
+        Tải ảnh gif và hiển thị lên label theo vòng lặp
         """
         if isinstance(im, str):
             """
-            Đọc các frame của gif từ đường dẫn được cung cấp
+            Đọc các frame của ảnh gif từ đường dẫn được cung cấp img
             """
             im = Image.open(im)
 
@@ -59,6 +47,7 @@ class LoadingGifLabel(customtkinter.CTkLabel):
 
                 # Thêm các khung hình của GIF vào biến lưu trữ
                 self.frames.append(ctk_img)
+                
                 im.seek(i)
         except EOFError:
             pass
@@ -100,51 +89,20 @@ class LoadingGifLabel(customtkinter.CTkLabel):
             self.loc %= len(self.frames)
             self.configure(image=self.frames[self.loc])
             self.after(self.delay, self.next_frame)
-    
-    def show_loading_popup_gif(self):
-        """
-        Hiển thị popup với tệp gif.
-        """
-        # Tạo label hiển thị gif
-        self.pack(fill='both', expand=True, padx = 2, pady = 2)
-        self.load(resource_path("assets\\images\\loading\\loading_gif.gif"))
-
-        # Khóa cửa sổ chính để chỉ có thể tương tác với cửa sổ con
-        self.parent.wm_attributes("-disabled", True)  
-        self.grab_set()  # Vô hiệu hóa các cửa sổ khác trong khi tải
-
-    def hide_loading_popup_gif(self):
-        """
-        Ẩn popup khi tải xong.
-        """
-        self.parent.wm_attributes("-disabled", False)  # Bật lại cửa sổ chính
-        if hasattr(self, 'loading_popup'):
-            # Gọi unload để giải phóng tài nguyên GIF trước khi đóng popup
-            if hasattr(self, 'unload'):
-                self.unload()
-
-            # Đảm bảo rằng loading_label được hủy bỏ đúng cách trước khi đóng popup
-            self.destroy()
-            del self  # Xóa tham chiếu tới label GIF
-
-            # Destroy popup
-            self.destroy() 
-            del self  # Xóa tham chiếu tới cửa sổ popup
-
 
 if __name__ == "__main__":
     # Create the root window using customtkinter
     root = customtkinter.CTk()
 
-    # Cập nhật vị trí cửa sổ ở giữa màn hình chính
-    root.geometry(f'900x900')
-
     # Set the desired resize dimensions (e.g., 150x150)
-    resize_dimensions = (150, 150)
+    resize_dimensions = (200, 200)
     
     # Create the customtkinter label with resizing option
     lbl = LoadingGifLabel(root, resize=resize_dimensions)
-    lbl.show_loading_popup_gif()
+    lbl.pack(fill='both', expand=True)
+    
+    # Load and display the gif
+    lbl.load("assets\\images\\loading\\loading_gif.gif")
     
     # Run the main loop
     root.mainloop()
