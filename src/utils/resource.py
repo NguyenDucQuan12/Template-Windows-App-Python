4fb87434f09e102d451a86d4bb8f93d7e903fdd4
Quan <nguyenducquan2001@gmail.com>
2025-07-18 16:16:04 +0700

    Cập nhật hàm truy vấn đường dẫn tệp tin

--- src/utils/resource.py
+++ src/utils/resource.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from utils.constants import IMAGE
 
 def resource_path(relative_path):
     """ Trả về đường dẫn đến file tài nguyên khi đóng gói với PyInstaller """
@@ -9,4 +10,19 @@ def resource_path(relative_path):
     except Exception:
         # Khi chạy từ mã nguồn Python
         base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+
+    full_path = os.path.join(base_path, relative_path)
+
+    # Kiểm tra tệp tin có tồn tại không trước khi trả về đường dẫn
+    if not os.path.exists(full_path):
+
+        # Kiểm tra đuôi tệp tin
+        if full_path.endswith(('.png', '.jpg', '.jpeg', '.ico')):
+            # Nếu là hình ảnh thì trả về đường dẫn mặc định
+            return IMAGE["DEFAULT_IMG"]
+        # Nếu không phải là hình ảnh thì trả về none
+        else:
+            # Nếu không phải là tệp tin văn bản thì trả về None
+            return None
+        
+    return full_path
