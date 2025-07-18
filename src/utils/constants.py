13d7939f9f923aa27cec424c33c978ebe566ef8c
Quan <nguyenducquan2001@gmail.com>
2025-07-18 16:17:39 +0700

    Cập nhật các giá trị hằng số

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,9 +1,38 @@
 # Tên phần mềm
-APP_NAME = ""
-APP_FOLDER_LOG = ""
+APP_NAME_SYSTEM = "DucQuan"
+APP_TITLE = "Nguyễn Đức Quân"
+APP_FOLDER_LOG = "DucQuanLog"
+APP_UPDATER = "Updater.exe"
 
 # Màu sắc
 
-# Đường dẫn tệp chứa thông tin đăng nhập
 
-CONFIG_FILE = 'src/config/login_information.json'
+# Các tên của navigation
+HOME_NAV = "Trang chủ"
+CHAT_NAV = "Trò chuyện"
+
+# Quyền hạn
+PERMISSION = {
+    "ADMIN": "Admin",
+    "USER": "User",
+    "GUEST": "Guest"
+}
+
+LIST_PERMISSION = ["Admin", "User", "Guest"]
+
+# Đường dẫn tới các tệp tin
+FILE_PATH = {
+    "LOGIN_CONFIG": 'src\\config\\login_information.json',
+    "UPDATE_CONFIG": "data\\update_information.json"
+}
+
+# Đường dẫn tới các hình ảnh mặc định
+IMAGE = {
+    "DEFAULT_IMG": "assets\\images\\default\\not_found_image.png",
+    "ICO_IMG": "assets\\images\\ico\\ico.ico",
+    "NAVIGATION_LOGO_IMG": "assets\\images\\navigation\\logo_navigation.png",
+    "HOME_NAVIGATION_LIGHT_IMG": "assets\\images\\navigation\\home_light.png",
+    "HOME_NAVIGATION_DARK_IMG": "assets\\images\\navigation\\home_dark.png",
+    "CHAT_NAVIGATION_LIGHT_IMG": "assets\\images\\navigation\\chat_light.png",
+    "CHAT_NAVIGATION_DARK_IMG": "assets\\images\\navigation\\chat_dark.png",
+}
